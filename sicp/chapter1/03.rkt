7877fb664c52c41762fdc28a53d91a47120fa7dd
Arthur Kuz <arthur.kuz@protonmail.com>
2023-07-01 13:01:06 +0300

    Chapter 1. Exercise 1.3 - Update solution

--- sicp/chapter1/03.rkt
+++ sicp/chapter1/03.rkt
@@ -4,15 +4,18 @@
 
 (define (square a) (* a a))
 
+(define (sum-of-squares a b) (+ (square a) (square b)))
+
 (define 
   (solution a b c)
   (cond
-    ((= (min a b c) a) (+ (square b) (square c)))
-    ((= (min a b c) b) (+ (square a) (square c)))
-    ((= (min a b c) c) (+ (square a) (square b)))))
+    ((= (min a b c) a) (sum-of-squares b c))
+    ((= (min a b c) b) (sum-of-squares a c))
+    ((= (min a b c) c) (sum-of-squares a b))))
 
 (check-equal? (solution 1 2 3) 13)
 (check-equal? (solution 2 1 3) 13)
 (check-equal? (solution 3 2 1) 13)
 (check-equal? (solution 1 2 2) 8)
 (check-equal? (solution 2 2 2) 8)
+(check-equal? (solution 1 10 100) 10100)
